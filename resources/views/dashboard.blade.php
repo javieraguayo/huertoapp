c056f838c421938a8c7bf7b7176b3574d8295661
javier <javier.aguayo19@gmail.com>
2020-11-07 02:15:40 -0300

    agregado migracion months y cambios en el dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,9 @@
 <x-app-layout>
 
     <x-slot name="header">
-        <ul class="flex justify-between">
-            <li class="mr-3">
-              <a class="inline-block border border-blue-500 rounded py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white" href="#">{{ __('Dashboard') }}</a>
-            </li>
-            <li class="mr-3">
-              <a class="inline-block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-2 px-4" href="#">Pill</a>
-            </li>
-            <li class="mr-3">
-              <a class="inline-block py-2 px-4 text-gray-400 cursor-not-allowed" href="#">Disabled Pill</a>
-            </li>
-          </ul>
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+        Crear 
+    </button>
     </x-slot>
 
     <div class="container my-12 mx-auto px-4 md:px-12">
